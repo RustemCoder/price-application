3b5fcc3a93c5d0d15fbd2285f532f8555ed7cfaa
Rustem Aisariyev <scorus0808@gmail.com>
2024-03-09 12:23:49 -0500

    added base application

---
+++ src/test/java/com/aisariyev/priceapp/PriceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisariyev.priceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
